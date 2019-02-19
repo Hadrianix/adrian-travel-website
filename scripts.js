a9cf5ff5a8cb9ddd08320d62c34c0ea29c3a7d8f
Hadrianix <33820513+Hadrianix@users.noreply.github.com>
2019-02-19 09:55:02 -0700

    added here and slider

--- scripts.js
+++ scripts.js
@@ -1,32 +1,32 @@
-// var sliderStuff = document.getElementById('slides');
+var sliderStuff = document.getElementById('slides');
 
-// var images = [
-//     'a',
-//     'b',
-//     'c',
-//     'd',
-//     'e'
+var images = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e'
 
-// ];
+];
 
-// var i = images.length;
+var i = images.length;
 
-// function nextImage() {
-//     if (i < images.length) {
-//         i = i + 1;
-//     } else {
-//         i = 1;
-//     }
-//     sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpeg>";
-// }
+function nextImage() {
+    if (i < images.length) {
+        i = i + 1;
+    } else {
+        i = 1;
+    }
+    sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpg>";
+}
 
-// function prevImage() {
-//     if (i < images.length + 1 && i > i) {
-//         i = 1 - 1;
-//     } else {
-//         i = 1;
-//     }
-//     sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpeg>";
-// }
+function prevImage() {
+    if (i < images.length + 1 && i > i) {
+        i = 1 - 1;
+    } else {
+        i = 1;
+    }
+    sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpg>";
+}
 
-// var slideCounter = setInterval(nextImage, 30000);
+var slideCounter = setInterval(nextImage, 30000);
