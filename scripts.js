a30ece0bc0ae27a96558fd4a06839d867d471e12
Hadrianix <33820513+Hadrianix@users.noreply.github.com>
2019-02-19 09:26:49 -0700

    added stuff

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// var sliderStuff = document.getElementById('slides');
+
+// var images = [
+//     'a',
+//     'b',
+//     'c',
+//     'd',
+//     'e'
+
+// ];
+
+// var i = images.length;
+
+// function nextImage() {
+//     if (i < images.length) {
+//         i = i + 1;
+//     } else {
+//         i = 1;
+//     }
+//     sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpeg>";
+// }
+
+// function prevImage() {
+//     if (i < images.length + 1 && i > i) {
+//         i = 1 - 1;
+//     } else {
+//         i = 1;
+//     }
+//     sliderStuff.innerHTML = "<img src=" + images[i - 1] + ".jpeg>";
+// }
+
+// var slideCounter = setInterval(nextImage, 30000);
